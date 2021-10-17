db7799a7251df760e5a799dc39204b0f36cc90ee
VasiliyMatr <vasiliymatrenin@gmail.com>
2021-10-17 17:57:07 +0300

    [VasiliyMatr][T01] : Fixed CMakeLists.txt a bit; Fixed triangles.cc a bit.

--- T01TRIANGLES/source/triangles.cc
+++ T01TRIANGLES/source/triangles.cc
@@ -7,28 +7,27 @@ using trPair = std::pair<geom3D::TriangleInfo, bool>;
 int main( int argc, char ** argv)
 {
     size_t trNum = 0;
-
     std::cin >> trNum;
 
     trPair * tr = new trPair [trNum];
 
-    for (int i = 0; i < trNum; ++i)
+    for (size_t i = 0; i < trNum; ++i)
     {
         geom3D::Point pts[3] = {};
-        for (int j = 0; j < 3; ++j)
+        for (size_t j = 0; j < 3; ++j)
         {
-            geom3D::fp_t coords [3] = {};
+            geom3D::fp_t coords[3] = {};
             std::cin >> coords[0] >> coords[1] >> coords[2];
-            pts [j] = geom3D::Point {coords[0], coords[1], coords[2]};
+            pts[j] = geom3D::Point {coords[0], coords[1], coords[2]};
         }
 
         tr[i].first = geom3D::TriangleInfo {pts[0], pts[1], pts[2]};
         tr[i].second = false;
     }
 
-    for (int i = 0; i < trNum; ++i)
+    for (size_t i = 0; i < trNum; ++i)
     {
-        for (int j = i; j < trNum; ++j)
+        for (size_t j = i; j < trNum; ++j)
             if (tr[i].first.crosses (tr[j].first))
             {
                 tr[i].second = true;
@@ -36,9 +35,9 @@ int main( int argc, char ** argv)
             }
     }
 
-    for (int i = 0; i < trNum; ++i)
+    for (size_t i = 0; i < trNum; ++i)
         if (tr[i].second)
-            std::cout << " " << i;
+            std::cout << i + 1 << " ";
 
     std::cout << std::endl;
 

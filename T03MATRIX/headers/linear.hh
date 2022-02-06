5de0f0faa86b62c415530df40ac05965a23c4dce
VasiliyMatr <vasiliymatrenin@gmail.com>
2022-02-06 22:23:31 +0300

    [T03] Few utils added: Added .clang-format Added CMakeLists.txt

--- T03MATRIX/headers/linear.hh
+++ T03MATRIX/headers/linear.hh
@@ -1,5 +1,6 @@
 
 #include <cstdlib>
+#include <utility>
 
 #ifndef LINEAR_HH_INCL
 #define LINEAR_HH_INCL
@@ -8,42 +9,42 @@ namespace linear
 {
 
 // Exceptions safe dynamical allocated block storage.
-template <class T>
-class VectorStorage
+template <class T> class VectorStorage
 {
   protected:
     size_t initialized_ = 0;
     T *data_ = nullptr;
 
-    VectorStorage(size_t size) : data_ {static_cast<T*>(::operator new(sizeof(T) * size))} {}
+    VectorStorage(size_t size) : data_{static_cast<T *>(::operator new(sizeof(T) * size))}
+    {
+    }
 
-    VectorStorage(VectorStorage&& toMove) noexcept
+    VectorStorage(VectorStorage &&toMove) noexcept
     {
         std::swap(initialized_, toMove.initialized_);
         std::swap(data_, toMove.data_);
     }
 
-    VectorStorage& operator=(VectorStorage&& toMove) noexcept
+    VectorStorage &operator=(VectorStorage &&toMove) noexcept
     {
         std::swap(initialized_, toMove.initialized_);
         std::swap(data_, toMove.data_);
         return *this;
     }
 
-    VectorStorage(const VectorStorage&) = delete;
-    VectorStorage& operator=(const VectorStorage&) = delete;
+    VectorStorage(const VectorStorage &) = delete;
+    VectorStorage &operator=(const VectorStorage &) = delete;
 
     ~VectorStorage()
     {
         for (size_t i = 0; i < initialized_; ++i)
             (data_ + i)->~T();
-        ::operator delete (data_);
+        ::operator delete(data_);
     }
 };
 
 // Dynamical array with elements access.
-template<class T>
-class Vector final : private VectorStorage<T>
+template <class T> class Vector final : private VectorStorage<T>
 {
     using VectorStorage<T>::initialized_;
     using VectorStorage<T>::data_;
@@ -52,22 +53,22 @@ class Vector final : private VectorStorage<T>
     size_t size_ = 0;
 
   public:
-    Vector(Vector&&) = default;
-    Vector &operator=(Vector&&) = default;
+    Vector(Vector &&) = default;
+    Vector &operator=(Vector &&) = default;
 
-    Vector(size_t size, const T& initVal = T{}) : VectorStorage<T>{size}, size_(size)
+    Vector(size_t size, const T &initVal = T{}) : VectorStorage<T>{size}, size_(size)
     {
         for (; initialized_ < size_; ++initialized_)
-            new(data_ + initialized_) T {initVal};
+            new (data_ + initialized_) T{initVal};
     }
 
-    Vector(const Vector& toCopy) : VectorStorage<T>{toCopy.size_}
+    Vector(const Vector &toCopy) : VectorStorage<T>{toCopy.size_}
     {
         for (; initialized_ < size_; ++initialized_)
-            new (data_ + initialized_) T {toCopy.data_[initialized_]};
+            new (data_ + initialized_) T{toCopy.data_[initialized_]};
     }
 
-    Vector &operator=(const Vector& toCopy)
+    Vector &operator=(const Vector &toCopy)
     {
         if (&toCopy == this)
             return *this;
@@ -90,22 +91,21 @@ class Vector final : private VectorStorage<T>
 };
 
 // Math vector with linear properties.
-template<size_t SIZE, class T>
-class MathVector final
+template <size_t SIZE, class T> class MathVector final
 {
     Vector<T> data_{SIZE};
 
   public:
-    T& operator[](size_t id)
+    T &operator[](size_t id)
     {
         return data_[id];
     }
-    const T& operator[](size_t id) const
+    const T &operator[](size_t id) const
     {
         return data_[id];
     }
 
-    MathVector& operator-() &
+    MathVector &operator-() &
     {
         for (size_t i = 0; i < SIZE; ++i)
             data_[i] = -data_[i];
@@ -117,45 +117,44 @@ class MathVector final
         return copy;
     }
 
-    MathVector& operator+=(const MathVector &sd) &
+    MathVector &operator+=(const MathVector &sd) &
     {
         for (size_t i = 0; i < SIZE; ++i)
             data_[i] += sd.data_[i];
     }
-    MathVector& operator-=(const MathVector &sd) &
+    MathVector &operator-=(const MathVector &sd) &
     {
         for (size_t i = 0; i < SIZE; ++i)
             data_[i] -= sd.data_[i];
     }
 };
 
-template<size_t SIZE, class T, class Convertable>
+template <size_t SIZE, class T, class Convertable>
 MathVector<SIZE, T> operator+(const MathVector<SIZE, T> &ft, const Convertable &sd)
 {
     MathVector<SIZE, T> copy{ft};
 
     return copy += sd;
 }
-template<size_t SIZE, class T, class Convertable>
+template <size_t SIZE, class T, class Convertable>
 MathVector<SIZE, T> operator+(const Convertable &ft, const MathVector<SIZE, T> &sd)
 {
     return sd + ft;
 }
 
-template<size_t SIZE, class T, class Convertable>
+template <size_t SIZE, class T, class Convertable>
 MathVector<SIZE, T> operator-(const MathVector<SIZE, T> &ft, const Convertable &sd)
 {
-    MathVector<SIZE, T> copy {sd};
+    MathVector<SIZE, T> copy{sd};
     return copy -= ft;
 }
-template<size_t SIZE, class T, class Convertable>
+template <size_t SIZE, class T, class Convertable>
 MathVector<SIZE, T> operator-(const Convertable &ft, const MathVector<SIZE, T> &sd)
 {
     return sd - ft;
 }
 
-template<size_t SIZE, class T>
-class SquareMatrix
+template <size_t SIZE, class T> class SquareMatrix
 {
     using row = MathVector<SIZE, T>;
 
@@ -163,9 +162,11 @@ class SquareMatrix
 
     struct ProxyRow
     {
-        row& row_;
+        row &row_;
 
-        ProxyRow(row& row) : row_{row} {}
+        ProxyRow(row &row) : row_{row}
+        {
+        }
 
         T &operator[](size_t id)
         {
@@ -181,11 +182,11 @@ class SquareMatrix
   public:
     ProxyRow operator[](size_t id)
     {
-        return ProxyRow {data_[id]};
+        return ProxyRow{data_[id]};
     }
     const ProxyRow operator[](size_t id) const
     {
-        return ProxyRow {data_[id]};
+        return ProxyRow{data_[id]};
     }
 };
 
--- T03MATRIX/sources/main.cc
+++ T03MATRIX/sources/main.cc
@@ -1,21 +1,21 @@
 
-#include <iostream>
 #include "linear.hh"
+#include <iostream>
 
 int main()
 {
     linear::SquareMatrix<2, int> mat;
 
-    for(size_t i = 0; i < 2; ++i)
-    for(size_t j = 0; j < 2; ++j)
-    {
-        mat[i][j] = (i + 1) * (j + 1);
-        std::cout << "value:" << (i + 1) * (j + 1) << std::endl;
-    }
+    for (size_t i = 0; i < 2; ++i)
+        for (size_t j = 0; j < 2; ++j)
+        {
+            mat[i][j] = (i + 1) * (j + 1);
+            std::cout << "value:" << (i + 1) * (j + 1) << std::endl;
+        }
 
-    for(size_t i = 0; i < 2; ++i)
-    for(size_t j = 0; j < 2; ++j)
-        std::cout << "mat["<<i<<"]["<<j<<"] = " << mat[i][j] << std::endl;
+    for (size_t i = 0; i < 2; ++i)
+        for (size_t j = 0; j < 2; ++j)
+            std::cout << "mat[" << i << "][" << j << "] = " << mat[i][j] << std::endl;
 
     return 0;
 }
